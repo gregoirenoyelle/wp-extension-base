cec2b8443f881a8a3a7db33647e04369d1808eab
Grégoire Noyelle <hello@gregoirenoyelle.com>
2018-12-21 22:42:36 +0100

    Ajout de notes pour le fichier enqueue

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -1,11 +1,15 @@
 <?php
 
-//* Générateur en ligne pour les script et les css
-// https://generatewp.com/register_script/
+/***
+ * Générateur en ligne pour les script et les css
+ * @link https://generatewp.com/register_script/
+ * @note Attention, dirname(__FILE__) et équivalant à __DIR__
+ * @note Bien penser à changer les préfixes "gn_" pour les fonctions
+ */
 
-/**************************
-* ENQUEUE POUR LE FRONT
-**************************/
+/***
+ * ENQUEUE POUR LE FRONT
+ */
 
 //* enqueue style front
 add_action('wp_enqueue_scripts','gn_styles_front', 99);
@@ -21,9 +25,10 @@ function gn_scripts_front() {
 	wp_enqueue_script('scripts-projet-front');
 }
 
-/**************************
-* ENQUEUE POUR LE BACK
-**************************/
+
+/***
+ * ENQUEUE POUR LE BACK
+ */
 
 //* enqueue style back
 add_action('admin_enqueue_scripts','gn_styles_back', 99);
